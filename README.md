50e0dc14464b1e907df74a857bb8dca7d1e30498
Mate Szurovecz <szuri92@Mates-MacBook-Pro.local>
2019-04-06 19:47:27 +0200

    add basic env

--- README.md
+++
@@ -1,1 +0,0 @@
-# kutulele-react-ui
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path')
+var webpack = require('webpack')
+
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+})
+
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.less$/,
+          use: [{
+            loader: 'style-loader' // creates style nodes from JS strings
+          }, {
+            loader: 'css-loader' // translates CSS into CommonJS
+          }, {
+            loader: 'less-loader' // compiles Less to CSS
+          }]
+        },
+      ]
+    },
+    plugins: [htmlPlugin],
+  };
